015579005800ea7342aec91ac86c75c2d065d63c
ChristianMoesl <s1036230@stud.sbg.ac.at>
2017-03-05 21:42:09 +0100

    Removed unused eslint comments

--- test/server/PoolController.js
+++ test/server/PoolController.js
@@ -1,9 +1,5 @@
-/* eslint-env node, mocha */
-
 import { expect } from 'chai';
-/* eslint-disable */
 import poolController from '../../src/server/PoolController';
-/* eslint-enable */
 
 describe('server/PoolController', () => {
     it('isn\'t initialised on startup', (done) => {
