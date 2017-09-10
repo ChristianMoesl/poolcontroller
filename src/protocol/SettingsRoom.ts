7b9f33d283f0d607fadff1136a41b94668f983a2
ChristianMoesl <christian.moesl@live.at>
2017-09-10 16:52:08 +0200

    configurable settings

--- src/protocol/SettingsRoom.ts
+++ src/protocol/SettingsRoom.ts
@@ -3,7 +3,8 @@ import { Room } from './Room';
 import { Logger, LoggerType } from '../services/Logger';
 import { SocketFactory, SocketFactoryType } from '../services/Socket';
 import { PoolSettings, PoolSettingsType } from '../services/PoolSettings';
-import { unknownVersion, Version, Message, Command } from 'poolcontroller-protocol';
+import { OperationMode } from '../system/OperationMode';
+import { unknownVersion, missingParameter, outOfRange, Version, Message, Command } from 'poolcontroller-protocol';
 
 @injectable()
 export class SettingsRoom extends Room {
@@ -17,22 +18,51 @@ export class SettingsRoom extends Room {
     }
 
     get(version: Version, data: Object) {
-        if (version.major === 1 && version.minor === 0) {
+        if (version.major === 1 && version.minor === 0)
             this.sendAck({
                 pumpTime: this.settings.getPumpTime(),
-                pumpInterval: this.settings.getPumpIntervall(),
+                pumpIntervall: this.settings.getPumpIntervall(),
+                targetTemperature: this.settings.getTargetTemperature(),
+                temperatureThreshold: this.settings.getTemperatureThreshold(),
+                operationMode: OperationMode[this.settings.getOperationMode()],
             });
-        } else {
+        else
             this.sendNak(unknownVersion);
-        }
     }
     
-    post(version: Version, data: Object) {
+    post(version: Version, data: any) {
+        let err = null;
 
-     /*   if (data === null) {
-            this.sendNak(missingParameter);
+        if (version.major === 1 && version.minor === 0) {
+            if (data === null)
+                this.sendNak(missingParameter);
+            else if (data.hasOwnProperty('pumpTime') && typeof(data.pumpTime) === 'number')
+                err = this.settings.setPumpTime(data.pumpTime);
+            else if (data.hasOwnProperty('pumpIntervall') && typeof(data.pumpIntervall) === 'number')
+                err = this.settings.setPumpIntervall(data.pumpIntervall);
+            else if (data.hasOwnProperty('operationMode') && typeof(data.operationMode) === 'string') {
+                if (data.operationMode === OperationMode[OperationMode.automatic])
+                    err = this.settings.setOperationMode(OperationMode.automatic);
+                else if (data.operationMode === OperationMode[OperationMode.manual])
+                    err = this.settings.setOperationMode(OperationMode.manual);
+                else
+                    err = RangeError(`operationMode has to be 'automatic' or 'manual', but was ${data.operationMode}`);
+            } else if (data.hasOwnProperty('targetTemperature') && typeof(data.targetTemperature) === 'number')
+                err = this.settings.setTargetTemperature(data.targetTemperature);
+            else if (data.hasOwnProperty('temperatureThreshold') && typeof(data.temperatureThreshold) === 'number')
+                err = this.settings.setTemperatureThreshold(data.temperatureThreshold);
+            else {
+                this.sendNak(missingParameter);
+                return;
+            }
         } else {
-            this.sendNak(notImplemented);
-        }*/
+            this.sendNak(unknownVersion);
+            return;
+        }
+
+        if (err === null)
+            this.sendAck();
+        else
+            this.sendNak(outOfRange, err.message);
     }
 }
